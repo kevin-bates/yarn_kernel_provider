b89472461c8046abef094710e7eb6a4a9eabb9ac
Kevin Bates <kbates4@gmail.com>
2019-06-28 17:32:07 -0700

    checkpoint: change to lifecycle manager, shared spec provider

--- yarn_kernel_provider/provider.py
+++ yarn_kernel_provider/provider.py
@@ -4,11 +4,5 @@ from remote_kernel_provider.provider import RemoteKernelProviderBase
 
 
 class YarnKernelProvider(RemoteKernelProviderBase):
-
     id = 'yarn'
-    kernels_dir = 'yarn_kernels'
-    expected_process_class = 'yarn_kernel_provider.yarn.YarnClusterProcessProxy'
-    supported_process_classes = [
-        'enterprise_gateway.services.processproxies.yarn.YarnClusterProcessProxy',
-        expected_process_class
-    ]
+    lifecycle_manager_class = 'yarn_kernel_provider.yarn.YarnLifecycleManager'
